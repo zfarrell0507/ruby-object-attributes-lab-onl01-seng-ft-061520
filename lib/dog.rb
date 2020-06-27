480f5fa9d32353ab71121a173240ac0995bf461b
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 22:55:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,3 +11,16 @@ end
 natalie = Dog.new("Natalie")
 natalie.name
 
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
