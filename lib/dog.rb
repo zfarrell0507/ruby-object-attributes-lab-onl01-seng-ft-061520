00c831f9b9ba5deb85ee792682be916c6bb3093c
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 23:02:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,15 @@ class Dog
     @name
   end
   
-  
+    def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+ 
+  def breed
+    @breed 
+  end
 end
  
 natalie = Dog.new
 natalie.name = "Natalie"
+natalie.breed = "Black Labrador"
